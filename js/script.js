cf186b496c6c05edc1f5a2ec1069758ef80d2ad4
Douglas Lemos <lemos.douglasoliveira@gmail.com>
2022-07-23 20:06:04 -0300

    Pokedex utilizando HTML, CSS e Javascript

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const som_botao = new Audio();
+const som_bg = new Audio();
+
+som_botao.src = './som/botao.mp3';
+som_bg.src = './som/bg.mp3';
+som_bg.volume = 0.2;
+
+
+
+const fetchPokemon = async (pokemon) => {
+    som_bg.play(); 
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async (pokemon) =>{
+
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fetchPokemon(pokemon);
+    
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];    
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not found';
+        pokemonNumber.innerHTML = '';
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+    som_botao.play();    
+   if(searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+   } 
+});
+buttonNext.addEventListener('click', () => {
+    som_botao.play();
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);    
+});
+
+renderPokemon(searchPokemon);
